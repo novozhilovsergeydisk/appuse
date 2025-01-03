4916cc914c85f4219c0444eefe41f01adee3582d
Сергей Новожилов <sergionov@mail.ru>
2025-01-03 06:10:17 -0500

    Initial commit

---
+++ js/script5.js
@@ -0,0 +1,4 @@
+// Открытие/закрытие меню бургера
+document.getElementById('burger').addEventListener('click', function () {
+    document.getElementById('nav-links').classList.toggle('active');
+});
